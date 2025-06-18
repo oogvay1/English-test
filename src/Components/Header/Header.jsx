7a85c74417f250cd6d59093a9346ecbd75fbf53b
oogvay1 <atalgat592@gmail.com>
2025-06-18 11:49:04 +0500

    Add background video

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,26 @@
+import { NavLink } from 'react-router-dom';
 import './Header.css'
 
 function Header() {
 
     return (
         <>
-            <h1>header</h1>
+            <header className='header'>
+                <div className="container">
+                    <div className="header-inner">
+                        <div className="header-logo">
+                            <NavLink to="/" >
+                                <img src="src/assets/imgs/11427f39c15a1b292c23c__2_-removebg-preview.png" alt="" />
+                                <h1>Quizz</h1>
+                            </NavLink>
+                        </div>
+
+                        <div className="header-mode">
+                            <i class="ri-sun-line sun"></i>
+                        </div>
+                    </div>
+                </div>
+            </header>
         </>
     );
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,25 @@
+import './Home.css'
 
 function Home() {
 
     return (
         <>
-            <h1>home</h1>
+            <section className='section'>
+                <div className="container">
+                    <div className="section-inner">
+                        <h1>Al Aziz Academy Test</h1>
+                        <p>Click the button below and start the preview test</p>
+                        <button className='btn'>
+                            <i class="ri-arrow-right-line"></i>
+                            <p>Start</p>
+                            <i class="ri-arrow-right-line"></i>
+                        </button>
+                    </div>
+                </div>
+            </section>
+            <video className='video' autoPlay muted loop width="100%">
+                <source src="src/assets/imgs/ace4939aefe5a2c294d49273022c3503.mp4" type="video/mp4" />
+            </video>
         </>
     );
 }
