1b7ea071c7b8a5f178e93720a964bbaf4cc78dbb
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-06-18 17:55:27 +0500

    Bacraund video ready

--- src/Pages/Test/Tests.jsx
+++ src/Pages/Test/Tests.jsx
@@ -1,12 +1,22 @@
-import './Test.css'
+import "./Test.css";
 
 function Tests() {
-
     return (
         <>
-            
+            <section>
+                <div className="test-user">
+                    <div className="container">
+                        <video className="video" autoPlay muted loop width="100%">
+                            <source
+                                src="src/assets/imgs/ace4939aefe5a2c294d49273022c3503.mp4"
+                                type="video/mp4"
+                            />
+                        </video>
+                    </div>
+                </div>
+            </section>
         </>
     );
 }
 
-export default Tests
+export default Tests;
