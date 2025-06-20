07ebca98afa291f9ec3a4b2f3ca5d09f8244facd
ibrohimovich884 <bkbekmirzayev@gmail.com>
2025-06-20 08:56:10 +0500

    Test style

--- src/Pages/Test/Tests.jsx
+++ src/Pages/Test/Tests.jsx
@@ -8,171 +8,21 @@ function Tests() {
                     <source src="src/assets/imgs/ace4939aefe5a2c294d49273022c3503.mp4" type="video/mp4" />
                 </video>
                 <div className="container">
-                    <div className="question-boxs">
-                        <div className="question-box">
+                    <div className="quiz-box">
+                        <div className="quiz-card">
                             <h1>Beginner</h1>
                             <div className="queestions">
                                 <h2>1 ......................... old are you?</h2>
-                                <div className="option">
+                                <div className="options">
                                     <label><input name='q1' type="radio" />How </label>
                                     <label><input name='q1' type="radio" />Who </label>
-                                    <label><input name='q1' type="radio" />What .......... </label>
+                                    <label><input name='q1' type="radio" />What </label>
                                 </div>
                             </div>
-                            <div className="queestions">
-                                <h2>2 Where ......................... ?</h2>
-                                <div className="option">
-                                    <label><input name='q2' type="radio" /> you from </label>
-                                    <label><input name='q2' type="radio" />you are from </label>
-                                    <label><input name='q2' type="radio" />you are from </label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>3 Our car isn’t fast. It’s very ......................... .</h2>
-                                <div className="option">
-                                    <label><input name='q3' type="radio" />weak </label>
-                                    <label><input name='q3' type="radio" />short </label>
-                                    <label><input name='q3' type="radio" />slow ..........</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>4 My mother likes Mel Gibson, but she ......................... Arnold Schwarzenegger.</h2>
-                                <div className="option">
-                                    <label><input name='q4' type="radio" />likes</label>
-                                    <label><input name='q4' type="radio" />don’t like </label>
-                                    <label><input name='q4' type="radio" />doesn’t like ..........</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>5 We usually watch a football match ......................... Saturday afternoon.</h2>
-                                <div className="option">
-                                    <label><input name='q5' type="radio" />in</label>
-                                    <label><input name='q5' type="radio" />on</label>
-                                    <label><input name='q5' type="radio" />at ..........</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>6 The programme starts at ten ......................... .</h2>
-                                <div className="option">
-                                    <label><input name='q6' type="radio" />o’clock</label>
-                                    <label><input name='q6' type="radio" />clocks</label>
-                                    <label><input name='q6' type="radio" />hours ..........</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>7 Where ......................... ?</h2>
-                                <div className="option">
-                                    <label><input name='q7' type="radio" />do you work</label>
-                                    <label><input name='q7' type="radio" />you do work</label>
-                                    <label><input name='q7' type="radio" />you work</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>8 That’s her over there – ......................... a green dress and a hat.</h2>
-                                <div className="option">
-                                    <label><input name='q8' type="radio" />she wears</label>
-                                    <label><input name='q8' type="radio" />she’s wearing</label>
-                                    <label><input name='q8' type="radio" />she wearing</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>9 People usually eat soup with ......................... .</h2>
-                                <div className="option">
-                                    <label><input name='q9' type="radio" />a knife</label>
-                                    <label><input name='q9' type="radio" />a fork</label>
-                                    <label><input name='q9' type="radio" />a spoon</label>
-                                </div>
-                            </div>
-                        </div>
-                         <div className="question-box">
-                            <h1>Elementary</h1>
-                            <div className="queestions">
-                                <h2>10 I ___ to classical music.</h2>
-                                <div className="option">
-                                    <label><input name='q10' type="radio" />never to listen</label>
-                                    <label><input name='q10' type="radio" />listen never</label>
-                                    <label><input name='q10' type="radio" />never listen</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>11 Would you like ___ coffee?</h2>
-                                <div className="option">
-                                    <label><input name='q11' type="radio" />other</label>
-                                    <label><input name='q11' type="radio" />another</label>
-                                    <label><input name='q11' type="radio" />some other</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>12 I haven’t ___ this photo before.</h2>
-                                <div className="option">
-                                    <label><input name='q12' type="radio" />see</label>
-                                    <label><input name='q12' type="radio" />saw</label>
-                                    <label><input name='q12' type="radio" />seen</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>13 I can’t see. Where are my ___?</h2>
-                                <div className="option">
-                                    <label><input name='q13' type="radio" />glasses</label>
-                                    <label><input name='q13' type="radio" />stamps</label>
-                                    <label><input name='q13' type="radio" />keys</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>14 I like ___ in the morning.</h2>
-                                <div className="option">
-                                    <label><input name='q14' type="radio" />that I work</label>
-                                    <label><input name='q14' type="radio" />working</label>
-                                    <label><input name='q14' type="radio" />work</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>15 ‘Was Debussy from France?’ ‘Yes, ___.’</h2>
-                                <div className="option">
-                                    <label><input name='q15' type="radio" />he were</label>
-                                    <label><input name='q15' type="radio" />was</label>
-                                    <label><input name='q15' type="radio" />he was</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>16 This isn’t my money. It’s ___.</h2>
-                                <div className="option">
-                                    <label><input name='q16' type="radio" />to you</label>
-                                    <label><input name='q16' type="radio" />your</label>
-                                    <label><input name='q16' type="radio" />yours</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>17 Tonight’s dinner is ___ than last night’s.</h2>
-                                <div className="option">
-                                    <label><input name='q17' type="radio" />more good</label>
-                                    <label><input name='q17' type="radio" />better</label>
-                                    <label><input name='q17' type="radio" />more better</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>18 They’re ___.</h2>
-                                <div className="option">
-                                    <label><input name='q18' type="radio" />big cars</label>
-                                    <label><input name='q18' type="radio" />cars bigs</label>
-                                    <label><input name='q18' type="radio" />big car</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>19 I’m Italian. ___ family are from Venice.</h2>
-                                <div className="option">
-                                    <label><input name='q19' type="radio" />Our</label>
-                                    <label><input name='q19' type="radio" />My</label>
-                                    <label><input name='q19' type="radio" />Her</label>
-                                </div>
-                            </div>
-                            <div className="queestions">
-                                <h2>20 What ___ do tomorrow?</h2>
-                                <div className="option">
-                                    <label><input name='q20' type="radio" />are you going</label>
-                                    <label><input name='q20' type="radio" />you going</label>
-                                    <label><input name='q20' type="radio" />are you going to</label>
-                                </div>
+                            <div className="buttons">
+                                <button id=''>Topa olmadim</button>
+                                <button id='next-btn'>Next</button>
+                                <button id=''>Tashlab ketish</button>
                             </div>
                         </div>
                     </div>
