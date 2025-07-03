89b796dd998a9e02d46357822d294dc7e3b58e18
oogvay1 <atalgat592@gmail.com>
2025-07-03 14:51:37 +0500

    Add backend folder

--- .vite/deps/react-dom.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-DG363TQQ.js";
-import "./chunk-3A6WRQ5K.js";
-export default require_react_dom();
--- .vite/deps/react_jsx-runtime.js
+++
@@ -1,5 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-F2QAYJ7Q.js";
-import "./chunk-3A6WRQ5K.js";
-export default require_jsx_runtime();
